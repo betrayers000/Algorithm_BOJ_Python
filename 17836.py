4652ebeb3dede84311f9682506a466d01660f4ab
jw <betrayers000@gmail.com>
2019-11-13 09:14:16 +0900

    17836 modify

--- 17836.py
+++ 17836.py
@@ -11,29 +11,31 @@ def bfs():
     q = [0] * (N * M)
     front, rear = -1, -1
     rear += 1
-    q[rear] = (0, 0, False)
+    q[rear] = (0, 0)
     ex, ey = N - 1, M - 1
     visited[0][0] = 1
+    ans = (N*M)+1
     while front != rear:
         front += 1
-        x, y, g = q[front]
+        x, y = q[front]
         if x == gram[0] and y == gram[1]:
-            g = True
+            temp = abs(x-ex) + abs(y-ey)
+            ans = visited[x][y] + temp
         if x == ex and y == ey:
-            if visited[x][y] - 1 > T:
+            res = min(ans, visited[x][y])
+            if res - 1 > T:
                 return "Fail"
-            return visited[x][y] - 1
+            return res - 1
         for k in range(4):
             nx, ny = x + dx[k], y + dy[k]
             if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0:
-                if not g and board[nx][ny] != 1:
+                if board[nx][ny] != 1:
                     rear += 1
-                    q[rear] = (nx, ny, g)
+                    q[rear] = (nx, ny)
                     visited[nx][ny] = visited[x][y] + 1
-                elif g:
-                    rear += 1
-                    q[rear] = (nx, ny, g)
-                    visited[nx][ny] = visited[x][y] + 1
+    if ans != (N*M)+1:
+        if ans-1 <= T:
+            return ans-1
     return "Fail"
 
 
