f91bb46b21f1ec16032423bf7e8e190c79966ba0
jw <betrayers000@gmail.com>
2019-11-15 17:50:21 +0900

    modfit

--- 1194.py
+++ 1194.py
@@ -2,52 +2,60 @@ import sys
 
 sys.stdin = open('input.txt', 'r')
 
+from collections import deque
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-key = ['a', 'b', 'c', 'd', 'e', 'f']
-door = ['A', 'B', 'C', 'D', 'E', 'F']
-
-keys = {}
-check = {
-    'A': False,
-    'B': False,
-    'C': False,
-    'D': False,
-    'E': False,
-    'F': False
-}
-
-def search(i, j, d):
+keys = 'abcdef'
+doors = 'ABCDEF'
+def search(i, j):
     global N, M
-    q = []
-    q.append((i, j))
-    visit = [[0] * M for _ in range(N)]
-    visit[i][j] = d
+    q = deque()
+    q.append((i, j, ""))
+    # q = [0] * (N*M*10)
+    # front, rear = -1, -1
+    # rear += 1
+    # q[rear] = (i, j, "")
+    visit[(i, j, "")] = 1
+    # while front != rear:
     while q:
-        x, y = q.pop(0)
-        if board[x][y] == '1':
-            return visit[x][y]
+        r = q.popleft()
+        # front += 1
+        # r = q[front]
+        x, y, key = r
         for k in range(4):
+            temp = key
             nx, ny = x + dx[k], y + dy[k]
-            if 0 <= nx < N and 0 <= ny < M and visit[nx][ny] == 0:
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] != "#":
                 fl = board[nx][ny]
-                if fl in key:
-                    keys[fl] = (nx, ny, visit[x][y] + 1)
-                elif fl not in door and fl != "#":
-                    q.append((nx, ny))
-                    visit[nx][ny] = visit[x][y] + 1
-    print(keys)
-
+                if visit.get((nx, ny, key)) == None:
+                    if fl in doors and fl.lower() not in key:
+                        continue
+                    else:
+                        if fl in keys and fl not in key:
+                            temp += fl
+                        visit[(nx, ny, temp)] = visit[r] + 1
+                        if (nx, ny) in end_point:
+                            return visit[r]
+                        # rear += 1
+                        # q[rear] = (nx, ny, temp)
+                        q.append((nx, ny, temp))
 
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
 res = 0
 si, sj = 0, 0
+visit = {}
+end_point = []
 for i in range(N):
     for j in range(M):
         if board[i][j] == '0':
             si, sj = i, j
-# search(si, sj, 1)
-# print(keys)
+        if board[i][j] == "1":
+            end_point.append((i, j))
+res = search(si, sj)
+if res == None:
+    print(-1)
+else:
+    print(res)
 
